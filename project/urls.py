8b8843191099d056beb4d61604ab9eb5b7dc2b66
GABRIEL ROMBOLA <rombolagabriel@gmail.com>
2023-01-09 23:50:05 -0300

    manage.py

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.urls import path
 from ejemplo_dos.views import (index, PostListar, PostCrear,
                                PostActualizar, PostBorrar, PostDetalle,
